52accb9d835686d453bb9b087159fecc0039c4ab
Samuel Gonzalez <xame2005@gmail.com>
2021-05-02 12:10:13 -0500

    [CREATED] onRemove event method

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useState } from "react";
 function App() {
   const { products } = data;
   const [cartItems, setCartItems] = useState([]);
+
   const onAdd = (product) => {
     const exist = cartItems.find((p) => p.id === product.id);
     if (exist) {
@@ -21,12 +22,29 @@ function App() {
     }
   };
 
+  const onRemove = (product) => {
+    const exist = cartItems.find((p) => p.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((p) => p.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((p) =>
+          p.id === product.id ? { ...exist, qty: exist.qty - 1 } : p
+        )
+      );
+    }
+  };
+
   return (
     <div className="App">
       <Header></Header>
       <div className="row">
         <Main onAdd={onAdd} products={products}></Main>
-        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
+        <Basket
+          onAdd={onAdd}
+          onRemove={onRemove}
+          cartItems={cartItems}
+        ></Basket>
       </div>
     </div>
   );

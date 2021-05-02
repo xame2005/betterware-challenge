cedc39c2dc3d6e663ba7c7be7ce0fc43579bc7cd
Samuel Gonzalez <xame2005@gmail.com>
2021-05-02 11:38:52 -0500

    [CREATED] Layout improvements and onAdd event method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import "./App.css";
-
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Basket from "./components/Basket";
 
 import data from "./data";
+import { useState } from "react";
 
 function App() {
   const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((p) => p.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((p) =>
+          p.id === product.id ? { ...exist, qty: exist.qty + 1 } : p
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
 
   return (
     <div className="App">
       <Header></Header>
       <div className="row">
-        <Main products={products}></Main>
-        <Basket></Basket>
+        <Main onAdd={onAdd} products={products}></Main>
+        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Product from "./Product";
 
 export default function Main(props) {
-  const { products } = props;
+  const { products, onAdd } = props;
   return (
     <main className="block col-2">
       <h2>Productos</h2>
-      <div className="row"></div>
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
     </main>
   );
 }
